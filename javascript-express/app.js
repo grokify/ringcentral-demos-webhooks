1d48c970e80bdef223aae9c3a97ab2c6d4aa5847
Anil Kumar <anil.akbp@gmail.com>
2016-06-23 01:21:22 -0700

    Adding WebHook URL to subscribe to all extensions

--- javascript-express/app.js
+++ javascript-express/app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 var https = require('https');
 var express = require('express');
 var app = express();
+var extensions = [];
 
 // Configure View and Handlebars
 app.set('views', __dirname + '/views');
@@ -30,12 +31,12 @@ if (useTls) {
     key: fs.readFileSync(process.env.MY_APP_TLS_PRIVATE_KEY),
     cert: fs.readFileSync(process.env.MY_APP_TLS_PUBLIC_CERT)
   }, app).listen(port, function() {
-    console.log('LISTEN_HTTPS ' + port);    
+    console.log('LISTEN_HTTPS ' + port);
   });
 } else if (! useTls) {
   server = require('http').Server(app);
   server.listen(port, function() {
-    console.log('LISTEN_HTTP ' + port);    
+    console.log('LISTEN_HTTP ' + port);
   });
 }
 
@@ -57,49 +58,50 @@ app.get('/', function(req, res) {
     }),
     redirect_uri: process.env.RC_APP_REDIRECT_URL,
     token_json: token_json,
-    webhook_uri: process.env.MY_APP_WEBHOOK_URL
+    webhook_uri: process.env.MY_APP_WEBHOOK_URL,
+    extensions: extensions
   });
 });
 
 app.get('/callback', function(req, res) {
   if (req.query.code) {
     rcsdk.platform()
-      .login({
-        code: req.query.code,
-        redirectUri: process.env.RC_APP_REDIRECT_URL
-      })
-      .then(function(response) {
-        console.log('logged_in');
-        res.send('');
-      })
-      .catch(function(e) {
-        console.log('ERR_CALLBACK ' + e.message  || 'Server cannot authorize user');
-        res.send('');
-      });
+        .login({
+          code: req.query.code,
+          redirectUri: process.env.RC_APP_REDIRECT_URL
+        })
+        .then(function(response) {
+          console.log('logged_in');
+          res.send('');
+        })
+        .catch(function(e) {
+          console.log('ERR_CALLBACK ' + e.message  || 'Server cannot authorize user');
+          res.send('');
+        });
   }
 });
 
 app.get('/hooks', function(req, res) {
   rcsdk.platform().loggedIn()
-    .then(function(status) {
-      rcsdk.platform()
-        .send({
-          method: 'GET',
-          url: '/subscription'
-        })
-        .then(function(response) {
-          console.log('logged_in');
-          var text = response.text();
-          res.send(text);
-        })
-        .catch(function(e) {
-          console.log('ERR ' + e.message  || 'Server cannot authorize user');
-          res.send('ERROR');
-        });
-    })
-    .catch(function(e) {
-      res.send("E_NOT_LOGGED_IN");
-    }); 
+      .then(function(status) {
+        rcsdk.platform()
+            .send({
+              method: 'GET',
+              url: '/subscription'
+            })
+            .then(function(response) {
+              console.log('logged_in');
+              var text = response.text();
+              res.send(text);
+            })
+            .catch(function(e) {
+              console.log('ERR ' + e.message  || 'Server cannot authorize user');
+              res.send('ERROR');
+            });
+      })
+      .catch(function(e) {
+        res.send("E_NOT_LOGGED_IN");
+      });
 });
 
 app.post('/hook', function(req, res) {
@@ -115,63 +117,86 @@ app.post('/hook', function(req, res) {
     console.log(JSON.stringify(req.body, null, 2));
   }
   rcsdk.platform().loggedIn()
-    .then(function(status) {
-      rcsdk.platform()
-        .send({
-          method: 'GET',
-          url: '/subscription'
-        })
-        .then(function(response) {
-          console.log('logged_in');
-          var text = response.text();
-          res.send(text);
-        })
-        .catch(function(e) {
-          console.log('ERR ' + e.message  || 'Server cannot authorize user');
-          res.send('ERROR');
-        });
-    })
-    .catch(function(e) {
-      res.send("E_NOT_LOGGED_IN");
-    }); 
+      .then(function(status) {
+        rcsdk.platform()
+            .send({
+              method: 'GET',
+              url: '/subscription'
+            })
+            .then(function(response) {
+              console.log('logged_in');
+              var text = response.text();
+              res.send(text);
+            })
+            .catch(function(e) {
+              console.log('ERR ' + e.message  || 'Server cannot authorize user');
+              res.send('ERROR');
+            });
+      })
+      .catch(function(e) {
+        res.send("E_NOT_LOGGED_IN");
+      });
 });
 
 app.post('/create_hook', urlencodedParser, function(req, res) {
   var requestBodyJson = req.body.requestBodyJson;
   var requestBodyData = JSON.parse(requestBodyJson);
   rcsdk.platform()
-    .post('/subscription', requestBodyData)
-    .then(function(response) {
-      console.log('Subscribed');
-      console.log(response.text());
-      res.send('SUB_SUCCESS ' + response.text());
-    })
-    .catch(function(e) {
-      console.log('Subscription Error');
-      console.log(e);
-      res.send(e);
-    })
+      .post('/subscription', requestBodyData)
+      .then(function(response) {
+        console.log('Subscribed');
+        console.log(response.text());
+        res.send('SUB_SUCCESS ' + response.text());
+      })
+      .catch(function(e) {
+        console.log('Subscription Error');
+        console.log(e);
+        res.send(e);
+      })
+});
+
+
+// Create WebHooks on all extensions
+app.post('/create_hook_all_extensions' , urlencodedParser, function(req, res) {
+
+  var extension_list = app.post( '/retreive_all_extensions', function( data ) {
+    alert('Extensions Retrieved');
+  });
+  //var requestBodyJson = req.body.requestBodyJson;
+  //var requestBodyData = JSON.parse(requestBodyJson);
+  //rcsdk.platform()
+  //    .post('/subscription', requestBodyData)
+  //    .then(function (response) {
+  //      console.log('Subscribed');
+  //      console.log(response.text());
+  //      res.send('SUB_SUCCESS ' + response.text());
+  //    })
+  //    .catch(function (e) {
+  //      console.log('Subscription Error');
+  //      console.log(e);
+  //      res.send(e);
+  //    })
 });
 
 app.post('/renew_hook', urlencodedParser, function(req, res) {
   var subscriptionId = req.body.subscriptionId || '';
   if (subscriptionId.match(/^[0-9a-f-]+$/)) {
     rcsdk.platform()
-      .send({
-        method: 'PUT',
-        url: '/subscription/' + subscriptionId,
-        headers: {'Content-Type': 'application/json'},
-        body: '{}'
-      })
-      .then(function(response) {
-        console.log('RENEW THEN');
-        res.send('RENEW THEN: ' + subscriptionId);
-      })
-      .catch(function(e) {
-        console.log('RENEW CATCH');
-        console.log(e);
-        res.send('RENEW CATCH: ' + e);
-      }) 
+        .send({
+          method: 'PUT',
+          url: '/subscription/' + subscriptionId,
+          headers: {'Content-Type': 'application/json'},
+          body: '{}'
+        })
+        .then(function(response) {
+          console.log('RENEW THEN');
+          res.send('RENEW THEN: ' + subscriptionId);
+        })
+        .catch(function(e) {
+          console.log('RENEW CATCH');
+          console.log(e);
+          res.send('RENEW CATCH: ' + e);
+        })
   } else {
     console.log('RENEW ELSE');
     res.sendStatus(400);
@@ -182,18 +207,63 @@ app.post('/delete_hook', urlencodedParser, function(req, res) {
   var subscriptionId = req.body.subscriptionId || '';
   if (subscriptionId.match(/^[0-9a-f-]+$/)) {
     rcsdk.platform()
-      .delete('/subscription/' + subscriptionId)
-      .then(function(response) {
-        console.log('DELETE THEN');
-        res.send('DELETE THEN: ' + subscriptionId);
-      })
-      .catch(function(e) {
-        console.log('DELETE CATCH');
-        console.log(e);
-        res.send('DELETE CATCH: ' + e);
-      }) 
+        .delete('/subscription/' + subscriptionId)
+        .then(function(response) {
+          console.log('DELETE THEN');
+          res.send('DELETE THEN: ' + subscriptionId);
+        })
+        .catch(function(e) {
+          console.log('DELETE CATCH');
+          console.log(e);
+          res.send('DELETE CATCH: ' + e);
+        })
   } else {
     console.log('DELETE ELSE');
     res.sendStatus(400);
   }
+});
+
+// To retrieve extensions
+app.post('/retreive_all_extensions', function(req, res) {
+  console.log('/retreive_all_extensions called');
+  rcsdk.platform().loggedIn()
+      .then(function(status) {
+        rcsdk.platform()
+            .send({
+              method: 'GET',
+              url: '/account/~/extension'
+            })
+            .then(function(response) {
+              console.log('retreiving extension list');
+              var text = response.text();
+              res.send(text);
+              var apiresponse = response.json();
+
+              for (var key in apiresponse.records) {
+
+                var extension_number = "";
+                if (apiresponse.records[key].hasOwnProperty('extensionNumber') && apiresponse.records[key].type == "User") {
+                  extension_number = parseInt(apiresponse.records[key].id);
+                  extensions.push(['/restapi/v1.0/account/account/~/extension/' + extension_number + '/presence?detailedTelephonyState=true&sipData=true']);
+                  extensions.push(['/restapi/v1.0/account/account/~/extension/' + extension_number + '/message-store']);
+                  extensions.push(['/restapi/v1.0/account/account/~/extension/' + extension_number + '/presence/line']);
+                  extensions.push(['/restapi/v1.0/account/account/~/extension/' + extension_number ]);
+                }
+
+                //console.log(extensions);
+              }
+              extensions.push(['/restapi/v1.0/subscription/~?threshold=86400&interval=3600']);
+              //res.send({
+              //  webhook_uri: process.env.MY_APP_WEBHOOK_URL,
+              //  extensions: extensions
+              //});
+            })
+            .catch(function(e) {
+              console.log('ERR ' + e.message  || 'Server cannot authorize user');
+              res.send('ERROR');
+            });
+      })
+      .catch(function(e) {
+        res.send("E_NOT_LOGGED_IN");
+      });
 });
